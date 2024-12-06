58dc40d2d4d43c2bc0974537c763f791bc529a11
kookrudals <kookrudals@gmail.com>
2024-12-06 09:02:30 +0900

    특정사원조회

--- src/main/java/app/api/controller/EmpAPIController.java
+++ src/main/java/app/api/controller/EmpAPIController.java
@@ -2,6 +2,7 @@ package app.api.controller;
 
 import java.util.Optional;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,8 +19,12 @@ public class EmpAPIController {
 	
 	private final EmpService empService;
 
-	@GetMapping("/emp/{empno}")
-	public Optional<Emp> getEmpByEmpno(@PathVariable Integer empno) {
-		return empService.getEmpByEmpno(empno);
+	@GetMapping("/emp/{empno}") 
+	public ResponseEntity<?> getEmpByEmpno(@PathVariable Integer empno) { Optional<Emp> emp = empService.getEmpByEmpno(empno); 
+	if (emp.isPresent()) { 
+		return ResponseEntity.ok(emp.get());
+		} else { 
+			return ResponseEntity.status(404).body("{\"msg\": \"사원정보가 존재하지 않습니다\"}"); 
+		} 
 	}
 }
